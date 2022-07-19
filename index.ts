0401f52746d79827eaacfa28edc1a87568946d01
ManojViasat <manoj.chinniah@viasat.com>
2022-07-19 10:34:21 -0700

    dsfgsfgfds

---
+++ index.ts
@@ -0,0 +1,21 @@
+import core from '@actions/core'
+import fs from 'fs/promises'
+import path from 'path'
+
+
+
+async function main() {
+
+    const directoryPath = core.getInput('directoryPath');
+    console.log(directoryPath);
+
+    const files = await fs.readdir(path.resolve("..", directoryPath))
+
+    //listing all files using forEach
+    var data = await Promise.all(files.map((file) => fs.readFile(file, 'utf-8')))
+
+    console.log(JSON.stringify(data))
+
+}
+
+main()
